b130858f31ae7ec10b8f96abd53fcdd1009e3c7b
AlexandrTarasyuk <alextarasyuk79@gmail.com>
2017-07-15 12:55:25 +0300

    Cleaning

--- app/src/main/java/com/example/denfox/supermegaapp/MainActivity.java
+++ app/src/main/java/com/example/denfox/supermegaapp/MainActivity.java
@@ -11,81 +11,14 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: Dont forget to declare your class here.
-    private SomeSecondClass someSecondClass;
-
-    private IgorMetlin igorMetlin;
-    private Mischenko mischenko;
-    private Peters peters;
-    private Nedomovny nedomovny;
-    private Sibichenko sibichenko;
-    private SerdunDmitro serdunDmitro;
-    private Volkov volkov;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 
-        initSomeClassStuff();
-
-        //TODO: Put your classes-init here!!!
-
-        igorMetlin = new IgorMetlin();
-        mischenko = new Mischenko();
-        peters = new Peters();
-        nedomovny = new Nedomovny();
-        sibichenko = new Sibichenko();
-        serdunDmitro=SerdunDmitro.getInstance();
     }
-
-    private void initSomeClassStuff() {
-        someSecondClass = new SomeSecondClass(true, false);
-
-        someSecondClass.doSomethingElse();
-        someSecondClass.doSomethingTruly();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        someSecondClass.doSomethingTruly();
-    }
-
 
 
 }
--- app/src/main/java/com/example/denfox/supermegaapp/Mischenko.java
+++
@@ -1,9 +0,0 @@
-package com.example.denfox.supermegaapp;
-
-/**
- * Created by Lux on 07.07.2017.
- */
-
-public class Mischenko {
-    int Lux = 1337;
-}
