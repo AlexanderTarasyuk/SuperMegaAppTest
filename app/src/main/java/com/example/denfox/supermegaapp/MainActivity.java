1570fff91706c7fadce96c4da006146faf466d71
AlexandrTarasyuk <alextarasyuk79@gmail.com>
2017-07-15 15:49:47 +0300

    BetaVersion

--- app/src/main/java/com/example/denfox/supermegaapp/MainActivity.java
+++ app/src/main/java/com/example/denfox/supermegaapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.denfox.supermegaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
+    private Button btnClear;
+    private Button btnSend;
+    private CheckBox checkBox;
+    private String textFromEditText;
 
 
     @Override
@@ -17,8 +27,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing buttons, edit text, checkbox
 
+        editText = (EditText) findViewById(R.id.editText);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnSend = (Button) findViewById(R.id.btnSend);
+        checkBox = (CheckBox) findViewById(R.id.chBox);
+//        textFromEditText=editText.getText().toString();
+    }
+
+    //
+    @Override
+    public void onClick(View view) {
+
+        if (view == null) {
+            return;
+        } else {
+
+            switch (view.getId()) {
+                case R.id.btnClear:
+                    editText.setText("");
+                    break;
+                case R.id.btnSend:
+                    if (editText.getText().toString().equals("")
+                            | !editText.getText().toString().contains("@")
+                            | !checkBox.isChecked()) {
+                        break;
+                    } else {
+                        Intent intent = new Intent(this, SecondActivity.class);
+                        intent.putExtra("text", editText.getText().toString());
+                        startActivityForResult(intent, 1);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        editText.setText(data.getStringExtra("name"));
+    }
 }
